2a773790fc91420bd6382ace8bb4133b1bb9a9d9
revione <oliiv@outlook.com>
2022-10-03 07:25:42 +0200

    slug

---
+++ src/routes/blog/[slug]/+page.server.js
@@ -0,0 +1,29 @@
+import { error } from '@sveltejs/kit';
+
+const getPostFromDatabase = (/** @type {string} */ slug) =>
+	new Promise((resolve, reject) => {
+		resolve({
+			id: 0,
+			slug,
+			title: 'etwas',
+			content: 'etwas content'
+		});
+	});
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ params }) {
+	const post = await getPostFromDatabase(params.slug);
+
+	if (params.slug === 'etwas') {
+		return {
+			title: 'Etwas!',
+			content: 'Welcome to our blog etwas. Lorem ipsum dolor sit amet...'
+		};
+	}
+
+	if (post) {
+		return post;
+	}
+
+	throw error(404, 'Not found');
+}
