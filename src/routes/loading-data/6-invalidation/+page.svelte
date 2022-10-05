a03df88fc3527b4cae1210ecd2c1f941266714bd
revione <oliiv@outlook.com>
2022-10-05 11:17:04 +0200

    clean some files

--- src/routes/loading-data/6-invalidation/+page.svelte
+++ src/routes/loading-data/6-invalidation/+page.svelte
@@ -1,9 +1,6 @@
-<div>
-	<h3>Invalidation</h3>
-</div>
+<svelte:head>
+	<title>Loading Data - Invalidation</title>
+	<meta name="description" content="Loading Data Invalidation how to use" />
+</svelte:head>
 
-<style>
-	div {
-		text-align: center;
-	}
-</style>
+<h3>Invalidation</h3>
