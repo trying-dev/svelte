26da795c45c1f746a321c27ab13711fb6f95a410
revione <oliiv@outlook.com>
2022-10-05 11:21:30 +0200

    max brothers

---
+++ src/routes/max-brothers/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Menu from '$lib/components/Menu.svelte'
+</script>
+
+<div>
+
+	<Menu list={['etwas', 'cualquiera']} />
+
+	Supuestamente deberia saltar a 404 
+
+	<slot />
+</div>
---
+++ src/routes/max-brothers/+page.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>Max brothers</title>
+	<meta name="description" content="Max brothers how to use" />
+</svelte:head>
+
+<div />
